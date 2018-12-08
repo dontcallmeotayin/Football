e26679acd712e3a3aa86a49e913d3b1688e77137
Jinwara Janejaneprasert <naon123@hotmail.com>
2018-12-08 23:49:55 +0700

    Add table and fill it

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
 import java.time.LocalDate;
 
 import application.MatchPane;
+import application.Tables;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.util.Pair;
 import logic.Club;
 import logic.Match;
 import logic.Stadium;
+import logic.Standings;
 
 public class Main extends Application{
 
+	private Standings data;
+	
 	@Override
 	public void start(Stage stage) {
 		VBox root = new VBox();
+		root.setPrefSize(700, 675);
+		
 //		Club ars = new Club("ARS");
 //		Club che = new Club("CHE");
 //		Pair<Club,Integer> a = new Pair<>(ars,1);
@@ -25,8 +33,23 @@ public class Main extends Application{
 //		Match test = new Match(c,a,date);
 //		MatchPane testpane = new MatchPane(test);
 //		root.getChildren().add(testpane);
+		
+		
+		Tables table = new Tables();
+		
+		root.getChildren().addAll(table);
+		
+		Media song = new Media(ClassLoader.getSystemResource("fifaSong.mp3").toString());
+		MediaPlayer bgm = new MediaPlayer(song);
+		bgm.setVolume(0.2);
+		bgm.setAutoPlay(true);
+		bgm.setCycleCount(MediaPlayer.INDEFINITE);
+		
+		root.setStyle("-fx-background-color: #CB918B;");
+		
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
+		stage.setResizable(false);
 		stage.show();
 	}
 	
