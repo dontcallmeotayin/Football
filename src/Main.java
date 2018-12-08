872bd4e147e6d5e0a7580995c26c613f464a99f3
unknown <rbbrki@gmail.com>
2018-12-08 15:47:14 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	@Override
+	public void start(Stage stage) {
+		VBox root = new VBox();
+		
+	}
+	
+	public static void main(String [] args) {
+		launch(args);
+	}
+	
+
+}
