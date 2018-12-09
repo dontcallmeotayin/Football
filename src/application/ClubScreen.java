8b17448d73b852671ca6d7fae9c5fa2263937553
unknown <rbbrki@gmail.com>
2018-12-09 22:26:02 +0700

    club

--- src/application/ClubScreen.java
+++ src/application/ClubScreen.java
@@ -7,11 +7,23 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import logic.Club;
 import logic.Player;
+import logic.PremierLeagueLogic;
 
 public class ClubScreen  extends Pane{
 		
 	public ClubScreen() {
-		// for adding clubpanes
+
+		PremierLeagueLogic logic = new PremierLeagueLogic();
+		ArrayList<Club> clubs = logic.getClubs();
+		ClubPane club1 = new ClubPane(clubs.get(0));
+		ClubPane club2 = new ClubPane(clubs.get(1));
+		ClubPane club3 = new ClubPane(clubs.get(2));
+		ClubPane club4 = new ClubPane(clubs.get(3));
+		ClubPane club5 = new ClubPane(clubs.get(4));
+		ClubPane club6 = new ClubPane(clubs.get(5));
+		
+		getChildren().addAll(club1,club2,club3,club4,club5,club6);
+
 	}
 
 }
